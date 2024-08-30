e4bad9846d8bfd25b86aa0a2a1132491cdf8bd84
lmgrill <lmgrill@gmail.com>
2024-08-30 16:18:42 -0400

    HNC-766: Updated LICENSE file and LICENSE file and LICENSE file and LICENSE file and LICENSE file and LICENSE file and LICENSE file

--- plugins/hl7/core/src/main/java/org/pentaho/di/ui/job/entries/hl7mllpack/HL7MLLPAcknowledgeDialog.java
+++ plugins/hl7/core/src/main/java/org/pentaho/di/ui/job/entries/hl7mllpack/HL7MLLPAcknowledgeDialog.java
@@ -1,6 +1,14 @@
-/*
- * Copyright (c) 2009 kJube.  All rights reserved.
- */
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 
 package org.pentaho.di.ui.job.entries.hl7mllpack;
 
